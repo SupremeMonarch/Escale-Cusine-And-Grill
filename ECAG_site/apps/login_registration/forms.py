6cf02fc22a411d48f808d3e9744c45560f36ef7b
n04h424 <143258261+n04h424@users.noreply.github.com>
2025-12-02 23:24:11 +0400

    Add files via upload

--- ECAG_site/apps/login_registration/forms.py
+++ ECAG_site/apps/login_registration/forms.py
@@ -5,23 +5,29 @@ from .models import Customer
 from django.contrib.auth.models import User
 
 class CustomerRegistrationForm(forms.ModelForm):
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'input-field', 'placeholder': 'Confirm Password'})
+    )
 
     class Meta:
         model = Customer
-        fields = ['First_Name', 'Last_Name', 'email', 'phone_number', 'Preferences', 'role', 'password']
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth', 'password']
         widgets = {
-            'password': forms.PasswordInput()
+            'first_name': forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Last Name'}),
+            'phone_number': forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Mobile Number'}),
+            'email': forms.EmailInput(attrs={'class': 'input-field', 'placeholder': 'Email'}),
+            'password': forms.PasswordInput(attrs={'class': 'input-field', 'placeholder': 'Password'}, render_value=False),
+            'address': forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Address'}),
+            'date_of_birth': forms.DateInput(attrs={'class': 'input-field', 'type': 'date'}),
         }
 
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
-
         if password != confirm_password:
             self.add_error('confirm_password', "Passwords do not match")
-
     
 class LoginForm(forms.Form):
     email = forms.EmailField(label="Email")
--- ECAG_site/apps/login_registration/models.py
+++ ECAG_site/apps/login_registration/models.py
@@ -20,23 +20,22 @@ class Customer(models.Model):
                                 )
     email_regex=RegexValidator(
                                     regex=r'(?i)^[a-z0-9._%+-]+@(gmail|hotmail|umail)\.com$',
-        
                                     message="Enter a valid Email."
                                 )
     customer_id = models.AutoField(primary_key=True)
-    First_Name=models.CharField(max_length=100,null=True)
-    Last_Name=models.CharField(max_length=100,null=True)
-    Preferences=models.CharField(max_length=10,choices=Preference_Choices)
+    first_name=models.CharField(max_length=100,null=True)
+    last_name=models.CharField(max_length=100,null=True)
+    address=models.CharField(max_length=100,null=True)
+    # Preferences=models.CharField(max_length=10,choices=Preference_Choices)
     password = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
     phone_number=models.CharField(validators=[phone_regex],max_length=20)
-    role=models.CharField(max_length=10,choices=Role_Choices)
+    # role=models.CharField(max_length=10,choices=Role_Choices)
     email=models.CharField(validators=[email_regex],max_length=100,unique=True)
     date_joined=models.DateTimeField(auto_now_add=True,null=True)
     
-    # HASH ONCE, SI HASH TWICE KK FANE
     def save(self, *args, **kwargs):
         # Hash password only when creating
         if not self.pk:  
             self.password = make_password(self.password)
-
         super().save(*args, **kwargs)
