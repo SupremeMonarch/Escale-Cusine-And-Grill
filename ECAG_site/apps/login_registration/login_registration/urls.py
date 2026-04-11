af2ee6e88d3f832f6a85fbf59268316e3996ff3e
n04h424 <143258261+n04h424@users.noreply.github.com>
2026-04-11 12:14:00 +0400

    Add files via upload

---
+++ ECAG_site/apps/login_registration/login_registration/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='login_registration'
+
+urlpatterns =   [
+                    path('register/',views.register,name="register"), # registration_view
+                    path('',views.login_view, name="login"),
+                    path('logout/', views.logout_view, name='logout')
+                ]
